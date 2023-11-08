27d98497af530d13ca1f5694add18956aaf33620
leleh5 <lelirodri50@gmail.com>
2023-11-08 02:00:53 -0300

    TP

---
+++ TP4/SairAction.java
@@ -0,0 +1,12 @@
+/*
+ * @author Leticia e Caroline
+ */
+
+import java.awt.event.*;
+
+public class SairAction
+        extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+}
